3dcab495a6a47ffd2ba03814a5aa5cc024a8ae02
BernardChoo <bernardchoowk@gmail.com>
2020-04-15 22:49:25 +0800

    1st commit.

--- part1/index.js
+++ part1/index.js
@@ -2,7 +2,7 @@ var Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 
 const oneSplitABI = require('./abis/onesplit.json');
-const onesplitAddress = "0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E"; // 1plit contract address on Main net
+const onesplitAddress = '0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E'; // 1plit contract address on Main net
 
 const fromToken = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'; // ETHEREUM
 const fromTokenDecimals = 18;
@@ -10,39 +10,56 @@ const fromTokenDecimals = 18;
 const toToken = '0x6b175474e89094c44da98b954eedeac495271d0f'; // DAI Token
 const toTokenDecimals = 18;
 
-const amountToExchange = 1
+const amountToExchange = 1;
 
-const web3 = new Web3('http://127.0.0.1:8545');
+const web3 = new Web3('<<INSERT MAINNET INFURA KEY>>');
 
 const onesplitContract = new web3.eth.Contract(oneSplitABI, onesplitAddress);
 
 const oneSplitDexes = [
-    "Uniswap",
-    "Kyber",
-    "Bancor",
-    "Oasis",
-    "CurveCompound",
-    "CurveUsdt",
-    "CurveY",
-    "Binance",
-    "Synthetix",
-    "UniswapCompound",
-    "UniswapChai",
-    "UniswapAave"
-]
-
-
-onesplitContract.methods.getExpectedReturn(fromToken, toToken, new BigNumber(amountToExchange).shiftedBy(fromTokenDecimals).toString(), 100, 0).call({ from: '0x9759A6Ac90977b93B58547b4A71c78317f391A28' }, function (error, result) {
-    if (error) {
-        console.log(error)
-        return;
-    }
-    console.log("Trade From: " + fromToken)
-    console.log("Trade To: " + toToken);
-    console.log("Trade Amount: " + amountToExchange);
-    console.log(new BigNumber(result.returnAmount).shiftedBy(-fromTokenDecimals).toString());
-    console.log("Using Dexes:");
-    for (let index = 0; index < result.distribution.length; index++) {
-        console.log(oneSplitDexes[index] + ": " + result.distribution[index] + "%");
-    }
-});
+	'Uniswap',
+	'Kyber',
+	'Bancor',
+	'Oasis',
+	'CurveCompound',
+	'CurveUsdt',
+	'CurveY',
+	'Binance',
+	'Synthetix',
+	'UniswapCompound',
+	'UniswapChai',
+	'UniswapAave'
+];
+/**
+ * 
+ *     function getExpectedReturn(
+        IERC20 fromToken,
+        IERC20 toToken,
+        uint256 amount,
+        uint256 parts,
+        uint256 disableFlags
+    )
+    
+ */
+onesplitContract.methods
+	.getExpectedReturn(
+		fromToken,
+		toToken,
+		new BigNumber(amountToExchange).shiftedBy(fromTokenDecimals).toString(),
+		100,
+		0
+	)
+	.call( function(error, result) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		console.log('Trade From: ' + fromToken);
+		console.log('Trade To: ' + toToken);
+		console.log('Trade Amount: ' + amountToExchange);
+		console.log(new BigNumber(result.returnAmount).shiftedBy(-fromTokenDecimals).toString());
+		console.log('Using Dexes:');
+		for (let index = 0; index < result.distribution.length; index++) {
+			console.log(oneSplitDexes[index] + ': ' + result.distribution[index] + '%');
+		}
+	});
